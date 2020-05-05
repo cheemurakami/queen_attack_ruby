82ba9e3594649b02cbc54d251611f0b8d355fe85
Chisato Murakami <kiwipom1031@gmail.com>
2020-05-04 17:44:56 -0700

    test all conditions

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,7 +1,19 @@
 class Array
-  def queen_attack?(x,y)
-   if (x != 0 && y === 0)
-   true
-   end
+  def queen_attack?(second_position_array)
+    first_position_array = self
+    first_x = first_position_array[0]
+    first_y = first_position_array[1]
+
+    second_x = second_position_array[0]
+    second_y = second_position_array[1]
+    if first_x - second_x == 0
+      true
+    elsif first_y - second_y == 0
+      true
+    elsif first_x - second_x == first_y - second_y
+      true
+    else
+      false
+    end
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -2,10 +2,20 @@ require('rspec')
 require('queen_attack')
 
 describe ("queen_attack?()") do
-  it('is true the coordinates are horizontally in line with each other') do
-    expect([1,1].queen_attack?([2,1])).to(eq(true))
+  it('is true the coordinates are vertically in line with each other') do
+    expect([1,0].queen_attack?([1,2])).to(eq(true))
   end
-  # it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
-  #   expect([1,1].queen_attack?([2, 3])).to(eq(false))
-  # end
+  it('is true the coordinates are horizontally in line with each other') do
+    expect([1,0].queen_attack?([3,0])).to(eq(true))
+  end
+  it('is true the coordinates are diagonally in line with each other') do
+    expect([1,1].queen_attack?([4,4])).to(eq(true))
+  end
+
+  it('is true the coordinates are diagonally in line with each other') do
+    expect([4,4].queen_attack?([1,1])).to(eq(true))
+  end
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([1,1].queen_attack?([2, 3])).to(eq(false))
+  end
 end
